f3aabaa1a4c302b3f011ec9f8bb1ab3252596bb6
Suszyński Krzysztof <krzysztof.suszynski@coi.gov.pl>
2018-03-23 18:00:08 +0100

    Resolve of root fs type fixed to support Debian 7

--- source/facter/partition-type.bash
+++ source/facter/partition-type.bash
@@ -3,23 +3,22 @@
 include facter.bash
 
 function facter.resolve.partition-type {
-  local lvm fstype
+  local lvm fstype rootfs_device
+  # get rootfs mount device
+  rootfs_device=$(mount | grep -o "^.* / " | awk '{print $1}')
   lvm=no
   # check if LVM tool lvs is present
   if which lvs 2>&1 >/dev/null; then
     # check for logical volumes
     if [[ "$(LANG=C lvs --noheadings -o +lv_path)" != *No\ volume\ groups\ found* ]]; then
-      # get rootfs mount device
-      local rootfs_device
-      rootfs_device=$(mount | grep -o "^.* / " | awk '{print $1}')
       # look for match, set LVM true if found
-      for lv_name in $(lvs --noheading | awk '{print $1}'); do
+      for lv_name in $(LANG=C lvs --noheading | awk '{print $1}'); do
         [[ "${rootfs_device}" == *${lv_name}* ]] && lvm=yes
       done
     fi
   fi
   facter.set lvm ${lvm}
-  fstype=$(LANG=C df -T | grep -E '\s/$' | awk '{print $2}' | sed -e 's:[0-9]::g')
+  fstype=$(LANG=C df -T | grep ${rootfs_device} | awk '{print $2}' | sed -e 's:[0-9]::g')
   facter.set fstype ${fstype}
   if ! [[ $fstype == 'ext' ]] && ! [[ $fstype == 'xfs' ]]; then
     logger.error "Filesystem ${fstype} is not supported, supported is only xfs or ext"
